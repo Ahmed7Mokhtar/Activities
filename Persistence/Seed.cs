2839664ee739956d19af1204549f18784b1dbc09
Ahmed Mokhtar <55771788+Ahmed7Mokhtar@users.noreply.github.com>
2023-03-26 05:52:00 +0200

    Added Attendees to activities (API)

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -7,7 +7,7 @@ namespace Persistence
     {
          public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
-            if(!userManager.Users.Any())
+            if(!userManager.Users.Any() && !context.Activities.Any())
             {
                 var users = new List<AppUser>
                 {
@@ -35,107 +35,224 @@ namespace Persistence
                 {
                     await userManager.CreateAsync(user, "2468Rr@");
                 }
-            }
-
-            if (context.Activities.Any()) 
-                return;
             
-            var activities = new List<Activity>
-            {
-                new Activity
+                var activities = new List<Activity>
                 {
-                    Title = "Past Activity 1",
-                    Date = DateTime.UtcNow.AddMonths(-2),
-                    Description = "Activity 2 months ago",
-                    Category = "drinks",
-                    City = "Cairo",
-                    Venue = "Al Khal Egyptian Restaurant",
-                },
-                new Activity
-                {
-                    Title = "Past Activity 2",
-                    Date = DateTime.UtcNow.AddMonths(-1),
-                    Description = "Activity 1 month ago",
-                    Category = "culture",
-                    City = "Minia",
-                    Venue = "Famous Burger",
-                },
-                new Activity
-                {
-                    Title = "Future Activity 1",
-                    Date = DateTime.UtcNow.AddMonths(1),
-                    Description = "Activity 1 month in future",
-                    Category = "culture",
-                    City = "Alex",
-                    Venue = "Ginger Asian Restaurant",
-                },
-                new Activity
-                {
-                    Title = "Future Activity 2",
-                    Date = DateTime.UtcNow.AddMonths(2),
-                    Description = "Activity 2 months in future",
-                    Category = "music",
-                    City = "Cairo",
-                    Venue = "Bab El-Sharq",
-                },
-                new Activity
-                {
-                    Title = "Future Activity 3",
-                    Date = DateTime.UtcNow.AddMonths(3),
-                    Description = "Activity 3 months in future",
-                    Category = "drinks",
-                    City = "Cairo",
-                    Venue = "El Fishawi",
-                },
-                new Activity
-                {
-                    Title = "Future Activity 4",
-                    Date = DateTime.UtcNow.AddMonths(4),
-                    Description = "Activity 4 months in future",
-                    Category = "drinks",
-                    City = "Alex",
-                    Venue = "El Fishawi",
-                },
-                new Activity
-                {
-                    Title = "Future Activity 5",
-                    Date = DateTime.UtcNow.AddMonths(5),
-                    Description = "Activity 5 months in future",
-                    Category = "drinks",
-                    City = "Cairo",
-                    Venue = "El Fishawi",
-                },
-                new Activity
-                {
-                    Title = "Future Activity 6",
-                    Date = DateTime.UtcNow.AddMonths(6),
-                    Description = "Activity 6 months in future",
-                    Category = "music",
-                    City = "Cairo",
-                    Venue = "El Fishawi",
-                },
-                new Activity
-                {
-                    Title = "Future Activity 7",
-                    Date = DateTime.UtcNow.AddMonths(7),
-                    Description = "Activity 2 months ago",
-                    Category = "travel",
-                    City = "Cairo",
-                    Venue = "El Fishawi",
-                },
-                new Activity
-                {
-                    Title = "Future Activity 8",
-                    Date = DateTime.UtcNow.AddMonths(8),
-                    Description = "Activity 8 months in future",
-                    Category = "film",
-                    City = "Cairo",
-                    Venue = "Cinema",
-                }
-            };
+                    new Activity
+                    {
+                        Title = "Past Activity 1",
+                        Date = DateTime.UtcNow.AddMonths(-2),
+                        Description = "Activity 2 months ago",
+                        Category = "drinks",
+                        City = "Cairo",
+                        Venue = "Al Khal Egyptian Restaurant",
+                        Attendees = new List<ActivityAttendee>
+                            {
+                                new ActivityAttendee
+                                {
+                                    AppUser = users[0],
+                                    IsHost = true
+                                }
+                            }
+                    },
+                    new Activity
+                    {
+                        Title = "Past Activity 2",
+                        Date = DateTime.UtcNow.AddMonths(-1),
+                        Description = "Activity 1 month ago",
+                        Category = "culture",
+                        City = "Minia",
+                        Venue = "Famous Burger",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[0],
+                                IsHost = true
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[1],
+                                IsHost = false
+                            },
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 1",
+                        Date = DateTime.UtcNow.AddMonths(1),
+                        Description = "Activity 1 month in future",
+                        Category = "culture",
+                        City = "Alex",
+                        Venue = "Ginger Asian Restaurant",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[2],
+                                IsHost = true
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[1],
+                                IsHost = false
+                            },
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 2",
+                        Date = DateTime.UtcNow.AddMonths(2),
+                        Description = "Activity 2 months in future",
+                        Category = "music",
+                        City = "Cairo",
+                        Venue = "Bab El-Sharq",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[0],
+                                IsHost = true
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[2],
+                                IsHost = false
+                            },
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 3",
+                        Date = DateTime.UtcNow.AddMonths(3),
+                        Description = "Activity 3 months in future",
+                        Category = "drinks",
+                        City = "Cairo",
+                        Venue = "El Fishawi",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[1],
+                                IsHost = true                            
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[0],
+                                IsHost = false                            
+                            },
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 4",
+                        Date = DateTime.UtcNow.AddMonths(4),
+                        Description = "Activity 4 months in future",
+                        Category = "drinks",
+                        City = "Alex",
+                        Venue = "El Fishawi",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[1],
+                                IsHost = true                            
+                            }
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 5",
+                        Date = DateTime.UtcNow.AddMonths(5),
+                        Description = "Activity 5 months in future",
+                        Category = "drinks",
+                        City = "Cairo",
+                        Venue = "El Fishawi",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[0],
+                                IsHost = true                            
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[1],
+                                IsHost = false                            
+                            },
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 6",
+                        Date = DateTime.UtcNow.AddMonths(6),
+                        Description = "Activity 6 months in future",
+                        Category = "music",
+                        City = "Cairo",
+                        Venue = "El Fishawi",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[2],
+                                IsHost = true                            
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[1],
+                                IsHost = false                            
+                            },
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 7",
+                        Date = DateTime.UtcNow.AddMonths(7),
+                        Description = "Activity 2 months ago",
+                        Category = "travel",
+                        City = "Cairo",
+                        Venue = "El Fishawi",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[0],
+                                IsHost = true                            
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[2],
+                                IsHost = false                            
+                            },
+                        }
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 8",
+                        Date = DateTime.UtcNow.AddMonths(8),
+                        Description = "Activity 8 months in future",
+                        Category = "film",
+                        City = "Cairo",
+                        Venue = "Cinema",
+                        Attendees = new List<ActivityAttendee>
+                        {
+                            new ActivityAttendee
+                            {
+                                AppUser = users[2],
+                                IsHost = true                            
+                            },
+                            new ActivityAttendee
+                            {
+                                AppUser = users[1],
+                                IsHost = false                            
+                            },
+                        }
+                    }
+                };
 
-            await context.Activities.AddRangeAsync(activities);
-            await context.SaveChangesAsync();
+                await context.Activities.AddRangeAsync(activities);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
